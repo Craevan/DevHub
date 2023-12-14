97f76da16861a30acc4d16c05d72071170bf7c95
Crevan <a.siverskii@gmail.com>
2023-12-14 22:39:34 +0300

    init commit

---
+++ src/main/java/dev/crevan/magazinus/MagazinusApplication.java
@@ -0,0 +1,13 @@
+package dev.crevan.magazinus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagazinusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagazinusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/crevan/magazinus/MagazinusApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.crevan.magazinus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagazinusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
